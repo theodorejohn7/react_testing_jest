6d32c86315c529198e78ac00102641df85060ba8
theodorejohn7 <theodorejohn.s@10decoders.in>
2022-05-25 14:54:44 +0530

    lazy loading complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 /* eslint-disable */
 
-
-
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import { Route, Routes } from 'react-router-dom';
 import Welcome from './components/login_form/Welcome';
@@ -13,6 +11,12 @@ import Register from './components/login_form/Register';
 import { AuthProvider } from './components/login_form/Utils/auth';
 import TestingApp from './components/apiTesting/testingApp';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+import lazyLoader from './components/lazyloading/LazyLoader';
+const ApiLoad = lazyLoader(
+  () => import('./components/apiTesting/testingApp'),
+  'loading the component'
+);
+
 export default class App extends React.Component {
   render() {
     return (
@@ -21,38 +25,31 @@ export default class App extends React.Component {
           <header>
             <MainHeader />
           </header>
-          {/* <ErrorBoundary  >
-            <Login   />
-          </ErrorBoundary> */}
-                  <ErrorBoundary>
-
-          <main>
-            <Routes>
-              <Route path="/" />
-              <Route
-                path="/login"
-                element={
-                    <Login />
-                }
-              />
-              <Route path="/register" element={<Register />} />
-
-              <Route
-                path="/welcome"
-                element={
-                  <RequireAuth>
-                    {' '}
-                    <Welcome />{' '}
-                  </RequireAuth>
-                }
-              />
-
-              <Route path="/testingapp" element={<TestingApp />} />
-            </Routes>
-
-          </main>
+          <ErrorBoundary>
+            <main>
+              <Routes>
+                <Route path="/" />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route
+                  path="/welcome"
+                  element={
+                    <RequireAuth>
+                      {' '}
+                      <Welcome />{' '}
+                    </RequireAuth>
+                  }                />
+                <Route
+                  path="/testingapp"
+                  element={
+                    <Suspense fallback={<h2>Component loading... </h2>}>
+                      <ApiLoad />
+                    </Suspense>
+                  }
+                />
+              </Routes>
+            </main>
           </ErrorBoundary>
-
         </div>
       </AuthProvider>
     );
